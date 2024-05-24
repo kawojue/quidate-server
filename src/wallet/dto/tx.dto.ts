b67c4563d4d7d08a23c00e8221538dedd3f0bb5f
Raheem Kawojue <kawojue08@gmail.com>
2024-05-24 13:34:17 +0100

    perf: auth

---
+++ src/wallet/dto/tx.dto.ts
@@ -0,0 +1,105 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { TransactionCurrency } from '@prisma/client'
+import {
+    IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, MinLength
+} from 'class-validator'
+
+
+class TxSourceDTO {
+    @ApiProperty({
+        example: 'USD',
+        description: 'The user balance source of the transaction.'
+    })
+    @IsEnum(TransactionCurrency)
+    tx_source: TransactionCurrency
+}
+
+export class AmountDTO {
+    @ApiProperty({
+        example: 2000.23,
+        description: 'The transaction amount'
+    })
+    @IsNotEmpty()
+    amount: number
+}
+
+
+export class InitiateWithdrawalDTO extends AmountDTO {
+    @ApiProperty({
+        example: '1234',
+    })
+    @IsString()
+    @MinLength(4, {
+        message: 'Transaction PIN is at least 4 digits'
+    })
+    @MaxLength(4, {
+        message: 'Transaction PIN is at most 4 digits'
+    })
+    @IsOptional()
+    pin?: string
+
+    @ApiProperty({
+        example: 'Normal token sha'
+    })
+    @IsString()
+    @IsOptional()
+    biometricToken?: string
+
+    // @ApiProperty({
+    //     example: "656b3.."
+    // })
+    // @IsOptional()
+    // @IsString()
+    // linkedBankId: string
+}
+
+export class InitiateLocalTransferDTO extends AmountDTO {
+    @ApiProperty({
+        example: '1234',
+    })
+    @IsString()
+    @MinLength(4, {
+        message: 'Transaction PIN is at least 4 digits'
+    })
+    @MaxLength(4, {
+        message: 'Transaction PIN is at most 4 digits'
+    })
+    @IsOptional()
+    pin?: string
+
+    @ApiProperty({
+        example: 'Normal token sha'
+    })
+    @IsString()
+    @IsOptional()
+    biometricToken?: string
+
+    @ApiProperty({
+        example: "food",
+        description: "This is optional."
+    })
+    @MaxLength(32, {
+        message: "Description is too long"
+    })
+    narration?: string
+}
+
+export class GetBankNameDTO {
+    @ApiProperty({
+        example: '044',
+        description: 'The bank code for the user'
+    })
+    @IsString()
+    @IsNotEmpty()
+    bankCode: string
+}
+
+export class FeeDTO extends TxSourceDTO {
+    @ApiProperty({
+        example: 2000.23,
+        description: 'The transaction amount'
+    })
+    @IsNumber()
+    @IsNotEmpty()
+    amount: number
+}

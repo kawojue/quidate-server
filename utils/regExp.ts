a717ed589cc791c57b084346981762c9cff72c2b
Raheem Muyiwa <kawojue08@gmail.com>
2024-05-26 09:31:20 +0100

    Update regExp.ts

--- utils/regExp.ts
+++ utils/regExp.ts
@@ -1,7 +1,7 @@
-const restrictedUser: string[] = [
+const restrictedUsername: string[] = [
     "profile", "admin", "account", "api", "root", "user",
     "signup", "login", "edit", "password", "reset", "logout",
-    "quidate", "account", "home", "reset", "auth", "main", "dashboard"
+    "quidate", "account", "home", "reset", "auth", "main", "dashboard", "cash", "transfer", "facebook", "twitter", "fund", "funds", "create"
 ]
 
-export const USER_REGEX = new RegExp(`^(?!(?:${restrictedUser.join('|')}))[a-zA-Z][a-zA-Z0-9-_]{2,14}$`)
+export const USER_REGEX = new RegExp(`^(?!(?:${restrictedUsername.join('|')}))[a-zA-Z][a-zA-Z0-9-_]{2,14}$`)

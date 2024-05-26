d525f5b653e48b939e478253dbb41e446cd548d4
Raheem Muyiwa <kawojue08@gmail.com>
2024-05-26 09:28:33 +0100

    Update regExp.ts

--- utils/regExp.ts
+++ utils/regExp.ts
@@ -4,4 +4,4 @@ const restrictedUser: string[] = [
     "quidate", "account", "home", "reset", "auth", "main", "dashboard"
 ]
 
-export const USER_REGEX = new RegExp(`^(?!(?:${restrictedUser.join('|')}))[a-zA-Z][a-zA-Z0-9-_]{2,23}$`)
+export const USER_REGEX = new RegExp(`^(?!(?:${restrictedUser.join('|')}))[a-zA-Z][a-zA-Z0-9-_]{2,14}$`)
